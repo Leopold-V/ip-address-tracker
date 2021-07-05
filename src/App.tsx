b180b86dde45cb268d59d24bc216c497e0cbb8cc
Leopold-V <leopoldV79@gmail.com>
2021-07-05 21:57:32 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { MainSection } from './components/MainSection'
+import { Search } from './components/Search'
 
 function App() {
 
   return (
-    <div className="App text-red-500 font-bold">
-      Hello
+    <div className="bg-pattern bg-cover bg-center text-red-400"  style={{backgroundImage: "url('images/pattern-bg.png')"}}>
+     <Search />
+     <MainSection /> 
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@
     purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          pattern: "url('/images/pattern-bg.png')"
+        }
+      }
     },
     variants: {},
     plugins: [],
