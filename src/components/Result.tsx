bc4fdc3759c9ddb4729bf0328b336a8dbd1e8623
Leopold-V <leopoldV79@gmail.com>
2021-07-08 15:47:10 +0200

    update

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -4,31 +4,31 @@ import { dataType } from '../type'
 export const Result = ({data}: {data: dataType}) => {
     return (
         <div 
-            className="absolute flex space-x-8 divide-x-2 divide-gray-200 lg:w-3/4 top-0 bg-white rounded-lg py-6 z-50" 
+            className="absolute w-3/4 top-0 flex flex-col lg:flex-row lg:space-x-8 lg:divide-x-2 lg:divide-gray-200 lg:items-center bg-white rounded-lg py-3 lg:py-6 z-50" 
             style={{left: '50%', transform: 'translate(-50%, -50%)'}}
         >
-            <div className="w-1/4">
-                <div className="px-10">
+            <div className="lg:w-1/4 text-center">
+                <div className="px-4 lg:px-10 py-1 lg:py-0">
                     <h3 className="py-2 text-xs font-medium text-gray-400">IP ADDRESS</h3>
-                    <div className="font-medium text-xl">{data.query}</div>
+                    <div className="font-medium text-base">{data.query}</div>
                 </div>
             </div>
-            <div className="w-1/4">
-                <div className="px-10">
+            <div className="lg:w-1/4 text-center">
+                <div className="px-4 lg:px-10 py-1 lg:py-0">
                     <h3 className="py-2 text-xs font-medium text-gray-400">LOCATION</h3>
-                    <div className="font-medium text-xl">{data.city} {data.zip}</div>
+                    <div className="font-medium text-base">{data.city} {data.zip}</div>
                 </div>
             </div>
-            <div className="w-1/4">
-                <div className="px-10">
+            <div className="lg:w-1/4 text-center">
+                <div className="px-4 lg:px-10 py-1 lg:py-0">
                     <h3 className="py-2 text-xs font-medium text-gray-400">TIMEZONE</h3>
-                    <div className="font-medium text-xl">{data.timezone}</div>
+                    <div className="font-medium text-base">{data.timezone}</div>
                 </div>
             </div>
-            <div className="w-1/4">
-                <div className="px-10">
+            <div className="lg:w-1/4 text-center">
+                <div className="px-4 lg:px-10 py-1 lg:py-0">
                     <h3 className="py-2 text-xs font-medium text-gray-400">ISP</h3>
-                    <div className="font-medium text-xl">{data.isp}</div>
+                    <div className="font-medium text-base">{data.isp}</div>
                 </div>
             </div>
         </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -33,13 +33,13 @@ export const Search = ({setData, map}: {setData: (data: dataType) => void, map: 
     }
 
     return (
-        <div className="h-60 bg-cover bg-center" style={{backgroundImage: "url('images/pattern-bg.png')"}}>
+        <div className="h-72 lg:h-60 bg-cover bg-center" style={{backgroundImage: "url('images/pattern-bg.png')"}}>
             <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-4 py-6">
                 <h1 className="text-white font-medium text-2xl">IP Address Tracker</h1>
                 <div className="flex items-center rounded-lg overflow-hidden">
                     <input
                         ref={input_ref}
-                        className="w-96 px-5 py-3 flex"
+                        className="sm:w-96 w-60 px-5 py-3 flex"
                         type="text"
                         placeholder="Search for any IP address or Domain"
                     />
